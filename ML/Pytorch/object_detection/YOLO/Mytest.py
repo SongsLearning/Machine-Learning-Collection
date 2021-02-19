77c9285aa9ca148c96295e0243268bed4278a53a
Song <217.Songs.SH@gmail.com>
2021-02-19 13:38:21 +0900

    print output

--- ML/Pytorch/object_detection/YOLO/Mytest.py
+++ ML/Pytorch/object_detection/YOLO/Mytest.py
@@ -18,6 +18,7 @@ from utils import (
 )
 from loss import YoloLoss
 import numpy as np
+
 LEARNING_RATE = 2e-5
 WEIGHT_DECAY = 0
 
@@ -29,12 +30,12 @@ optimizer = optim.Adam(
 )
 loss_fn = YoloLoss()
 
-
 IMG_DIR = "data/images"
 LABEL_DIR = "data/labels"
 NUM_WORKERS = 0
 BATCH_SIZE = 1
 PIN_MEMORY = True
+
 
 class Compose(object):
     def __init__(self, transforms):
@@ -47,7 +48,7 @@ class Compose(object):
         return img, bboxes
 
 
-transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor(),])
+transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor(), ])
 
 test_dataset = VOCDataset(
     "data/test.csv", transform=transform, img_dir=IMG_DIR, label_dir=LABEL_DIR,
@@ -56,15 +57,16 @@ test_dataset = VOCDataset(
 test_dataset.__getitem__(0)
 
 test_loader = DataLoader(
-        dataset=test_dataset,
-        batch_size=BATCH_SIZE,
-        num_workers=NUM_WORKERS,
-        pin_memory=PIN_MEMORY,
-        shuffle=True,
-        drop_last=True,
-    )
-def train_fn(train_loader, model, optimizer, loss_fn):
+    dataset=test_dataset,
+    batch_size=BATCH_SIZE,
+    num_workers=NUM_WORKERS,
+    pin_memory=PIN_MEMORY,
+    shuffle=True,
+    drop_last=True,
+)
 
+
+def train_fn(train_loader, model, optimizer, loss_fn):
     mean_loss = []
 
     for batch_idx, (x, y) in enumerate(train_loader):
@@ -81,7 +83,56 @@ def train_fn(train_loader, model, optimizer, loss_fn):
         loss.backward()
         optimizer.step()
 
-    print(f"Mean loss was {sum(mean_loss)/len(mean_loss)}")
+    print(f"Mean loss was {sum(mean_loss) / len(mean_loss)}")
 
-for i in range(300):
-    train_fn(test_loader, model,optimizer,loss_fn)
+
+for i in range(30):
+    train_fn(test_loader, model, optimizer, loss_fn)
+
+img = None
+for x, y in test_loader:
+    img = x.to(DEVICE)
+
+out = model(img)
+
+predictions = out.reshape(-1, 7, 7, 20 + 2 * 5)
+
+fn_tonumpy = lambda x: x.to('cpu').detach().numpy().transpose(0, 2, 3, 1)
+input = fn_tonumpy(img)
+input_ = input[0]
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from PIL import Image
+
+# Create figure and axes
+fig, ax = plt.subplots()
+
+# Display the image
+ax.imshow(input_)
+
+
+def draw(p):
+    for idx_i in range(7):
+        for idx_j in range(7):
+            out = p[idx_i, idx_j]
+            box = out[1:5]
+
+            con = box[0]
+            print(con)
+            if con > 0.35:
+                box[0] = (box[0] + idx_j) / 7
+                box[1] = (box[1] + idx_i) / 7
+                box = box * 488
+                rect = patches.Rectangle((box[0], box[1]), 30, 30, linewidth=1, edgecolor='r', facecolor='none')
+
+                # Add the patch to the Axes
+                ax.add_patch(rect)
+
+p = predictions.squeeze(0)[...,20:25]
+draw(p)
+p = predictions.squeeze(0)[...,25:30]
+draw(p)
+
+
+plt.show()
